2137c605f910df8c5b47b6ac3f8c9790d9c992b9
9ummy9 <148167964+9ummy9@users.noreply.github.com>
2023-12-13 15:48:33 +0900

    Feat: 버튼 클릭시 토스트 구현

---
+++ src/components/common/Button/ToastButton.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import Toast from './Toast.jsx';
+import linkIcon from '../../../images/linkBrown.svg';
+
+export default function ToastButton() {
+  const [toastMessage, setToastMessage] = useState('');
+
+  const handleButtonClick = () => {
+    setToastMessage('URL이 복사되었습니다');
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleButtonClick}>
+        <img src={linkIcon} alt="Link Icon" />
+      </button>
+
+      {toastMessage && <Toast pop={toastMessage} />}
+    </div>
+  );
+}
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,11 @@
+import ToastButton from '../../components/common/Button/ToastButton.jsx';
 import styles from './HomePage.module.css';
-import Toast from '../../components/common/Button/Toast';
 
 export default function App() {
   return (
     <div className={styles.home}>
       home
-      <Toast pop="URL이 복사되었습니다" />
+      <ToastButton />
     </div>
   );
 }
